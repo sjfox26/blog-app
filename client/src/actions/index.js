f0b6b1de7b694f864ca82f30849142ebc1f32610
Scott Fox <scottfox87@gmail.com>
2019-01-07 17:53:38 -0600

    set up basic actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,50 @@
+import articles from '../apis/articles';
+import { CREATE_ARTICLE, FETCH_ARTICLES, FETCH_ARTICLE, DELETE_ARTICLE, EDIT_ARTICLE } from "./types";
+import history from '../history'
+
+/*export const signIn = (userId) => {
+    return {
+        type: SIGN_IN,
+        payload: userId
+    };
+};
+
+export const signOut = () => {
+    return {
+        type: SIGN_OUT
+    };
+};*/
+
+export const createArticle = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await articles.post('/articles', { ...formValues, userId });
+
+    dispatch({ type: CREATE_ARTICLE, payload: response.data });
+    history.push('/');
+};
+
+export const fetchArticles = () => async dispatch => {
+    const response = await articles.get('/articles');
+
+    dispatch({ type: FETCH_ARTICLES, payload: response.data });
+};
+
+export const fetchArticle = (id) => async dispatch => {
+    const response = await articles.get(`/articles/${id}`);
+
+    dispatch({ type: FETCH_ARTICLE, payload: response.data });
+};
+
+export const editArticle = (id, formValues) => async dispatch => {
+    const response = await articles.patch(`/articles/${id}`, formValues);
+
+    dispatch({ type: EDIT_ARTICLE, payload: response.data });
+    history.push('/');
+};
+
+export const deleteArticle = (id) => async dispatch => {
+    await articles.delete(`/articles/${id}`);
+
+    dispatch({ type: DELETE_ARTICLE, payload: id });
+    history.push('/');
+};
---
+++ client/src/actions/types.js
@@ -0,0 +1,7 @@
+//export const SIGN_IN = 'SIGN_IN';
+//export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_ARTICLE';
+export const FETCH_STREAMS = 'FETCH_ARTICLES';
+export const FETCH_STREAM = 'FETCH_ARTICLE';
+export const DELETE_STREAM = 'DELETE_ARTICLE';
+export const EDIT_STREAM = 'EDIT_ARTICLE';
